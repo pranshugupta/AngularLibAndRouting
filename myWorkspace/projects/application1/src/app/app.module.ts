1cdb5a7f4fe496e864706ee27f1a9f62b3974723
Pranshu Gupta <pranshugupta18@gmail.com>
2019-08-05 20:25:27 +0530

    initial

--- myWorkspace/projects/application1/src/app/app.module.ts
+++ myWorkspace/projects/application1/src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { App1Comp1Component } from './app1-comp1/app1-comp1.component';
-import { App1Comp2Component } from './app1-comp2/app1-comp2.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    App1Comp1Component,
-    App1Comp2Component
   ],
   imports: [
     BrowserModule,
--- myWorkspace/projects/application2/src/app/app.module.ts
+++ myWorkspace/projects/application2/src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { App2Comp1Component } from './app2-comp1/app2-comp1.component';
-import { App2Comp2Component } from './app2-comp2/app2-comp2.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    App2Comp1Component,
-    App2Comp2Component
   ],
   imports: [
     BrowserModule,
