9b2008dbe5539d4be7d1aed873bb7cce30d5e57c
Pranshu Gupta <pranshugupta18@gmail.com>
2019-10-14 19:29:13 +0530

    updated

--- myWorkspace/projects/app1/src/app/app.module.ts
+++ myWorkspace/projects/app1/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
